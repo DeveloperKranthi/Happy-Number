30f41f30956ae98368a3f1f31fc102a4bf17329a
G Krnathi Kumar <49958256+DeveloperKranthi@users.noreply.github.com>
2021-02-03 18:27:42 +0530

    Add files via upload

---
+++ HapppyNumber.js
@@ -0,0 +1,15 @@
+var isHappy = function (n) {
+  let set = new Set();
+  while (!set.has(n)) {
+    set.add(n);
+    let arr = n.toString();
+    let squarsSum = 0;
+    for (let i = 0; i < arr.length; i++) {
+      squarsSum += Math.pow(+arr[i], 2);
+    }
+    if (squarsSum === 1) return true;
+    n = squarsSum;
+  }
+  return false;
+};
+isHappy(19);
